79468a439c93031377e032a853c0cb9e41e05b57
sbigelo <sean.bigelow1@gmail.com>
2021-12-16 15:39:11 -0600

    added a simple counterwith hooks

--- src/App.js
+++ src/App.js
@@ -1,62 +1,16 @@
 import React, { useState } from 'react'
-import './App.css'
 
-function Todo({ todo, index }) {
-  return(
-    <div className='todo'>
-      {index + 1}. {todo.text}
-    </div>
-  )
-}
+const App = () => {
+  const [counter, setCounter] = useState(0)
 
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState('')
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    if(!value) return
-    addTodo(value)
-    setValue('')
+  const increment = () => {
+    setCounter(counter + 1)
   }
 
-  return(
-    <form onSubmit={handleSubmit}>
-      <input type='text' className='input' value={value} onChange={e => setValue(e.target.value)}/>
-    </form>
-  )
-}
-
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      text: 'Learn about react',
-      isCompleted: false
-    },
-    {
-      text: 'Meet friend for lunch',
-      isCompleted: false
-    },
-    {
-      text: 'Build really cool todo app',
-      isCompleted: false
-    }
-  ])
-
-  const addTodo = text => {
-    const newTodos = [...todos, { text }]
-    setTodos(newTodos)
-  }
-
-  return (
-    <div className='app'>
-      <div className='todo list'>
-        {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
-    </div>
-  )
+  return <div>
+    {counter} 
+    <button onClick={increment}>Increment</button>
+  </div>
 }
 
 export default App
