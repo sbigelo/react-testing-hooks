04711ceea88dd1a3a1cc73f13782f3b2cbff5e82
sbigelo <sean.bigelow1@gmail.com>
2021-12-17 14:41:57 -0600

    added a decrement and conditional that it cant be below 0

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState('Pedro')
 
   const increment = () => {
     setCounter(counter + 1)
   }
 
+  const changeIt = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const decrement = () => {
+    if (counter >= 1) { 
+      setCounter(counter - 1)
+    }  
+  }
+
+
+ 
+
   return <div>
+    <input onChange={changeIt} placeholder='Enter it' />
+    {inputValue}
+    <br></br>
+    <br></br>
+    <br></br>
     {counter} 
     <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
   </div>
 }
 
