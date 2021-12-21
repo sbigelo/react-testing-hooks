786eac8fcc46f3bd3727690e4ec57c6c948d90d4
sbigelo <sean.bigelow1@gmail.com>
2021-12-21 11:49:26 -0500

    trying out useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
+
+  const reducer = (state, action) => {
+switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+    case "toggleShowText":
+      return { count: state.count, showText: !state.showText };
+    default:
+      return state;
+    }
+  }
+
+  
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [inputValue, setInputValue] = useState('Pedro')
+  const [state, dispatch] = useReducer(reducer, {count: 0, showText: true })
+
+
 
   const increment = () => {
     setCounter(counter + 1)
@@ -18,7 +34,10 @@ const App = () => {
     }  
   }
 
-
+  let onChange = (e) => {
+    const newValue = e.target.value
+    setInputValue(newValue)
+  }
  
 
   return <div>
@@ -30,6 +49,19 @@ const App = () => {
     {counter} 
     <button onClick={increment}>Increment</button>
     <button onClick={decrement}>Decrement</button>
+    <br></br>
+    <br></br>
+    <br></br>
+    <h1>{state.count}</h1>
+  <button
+    onClick={() => {
+     dispatch({type: "INCREMENT"})
+     dispatch({type: "toggleShowText"})
+    }}
+  >Click me</button>
+  {state.showText && <p>Even</p>}
+
+
   </div>
 }
 
